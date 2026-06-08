06fbdac10c221e8db4212a470ce702c0c7bbdb86
Brandon Payton <brandon@happycode.net>
2026-06-07 22:13:12 -0400

    Avoid browser CI refetches without package rebuilds

--- scripts/dev-shell.sh
+++ scripts/dev-shell.sh
@@ -87,7 +87,6 @@ nix_develop=(
     --keep WASM_POSIX_BINARY_INDEX_URL \
     --keep WASM_POSIX_USE_PR_STAGING \
     --keep WASM_POSIX_FETCH_SKIP_PKGS \
-    --keep WASM_POSIX_SKIP_BROWSER_FETCH \
     --keep WASM_POSIX_SYSROOT \
     --keep WASM_POSIX_LLVM_DIR \
     --accept-flake-config \
