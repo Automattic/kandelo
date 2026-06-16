8c77b66d04e3567920d658a2c131a17a7ea20657
brandonpayton <brandonpayton@users.noreply.github.com>
2026-06-16 03:56:31 +0000

    deploy: f0f99c396f2ba63a66592176e3823d07499422ed

---
+++ assets/__vite-browser-external-0rkbrCr9.js
@@ -0,0 +1,5 @@
+import { t as r } from "./browser-kernel-worker-entry-DcwYB1m0.js";
+var e = r((r, e) => {
+	e.exports = {};
+});
+export default e();
---
+++ assets/__vite-browser-external-B10XlTMB.js
@@ -0,0 +1,5 @@
+import { t as r } from "./network-demo-worker-YyNKFC70.js";
+var o = r((r, o) => {
+	o.exports = {};
+});
+export default o();
